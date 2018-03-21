413c8caa1c6e491920363871512ea7f5218a2bb1
Mitsunori YOSHIDA <marbocub@gmail.com>
2018-03-21 21:50:58 +0900

    MariaPrintTray has been added.

---
+++ MariaPrintTray/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MariaPrintTray
+{
+    class Watcher
+    {
+        private NotifyIcon notify1;
+
+        public Watcher()
+        {
+            string path = null;
+            try
+            {
+                path = (string)Microsoft.Win32.Registry.GetValue(Properties.Resources.RegKeyPortMonitorSettings, Properties.Resources.RegValueDirectory, null);
+            }
+            catch (Exception ex)
+            {
+                path = null;
+            }
+            if (path == null)
+            {
+                path = System.Environment.GetEnvironmentVariable("TEMP");
+            }
+            FileSystemWatcher watcher1 = new FileSystemWatcher();
+            watcher1.Path = path;
+            watcher1.NotifyFilter = NotifyFilters.FileName;
+            watcher1.Filter = @"*.fin";
+            watcher1.EnableRaisingEvents = true;
+            watcher1.Created += new FileSystemEventHandler(watcher1_Created);
+
+            ContextMenuStrip menu1 = new ContextMenuStrip();
+            ToolStripMenuItem menuItemAbout = new ToolStripMenuItem();
+            ToolStripMenuItem menuItemExit = new ToolStripMenuItem();
+            menu1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {menuItemAbout, menuItemExit});
+            menuItemAbout.Text = Properties.Resources.MenuTextAbout;
+            menuItemAbout.Click += new System.EventHandler(menuItemAbout_Click);
+            menuItemExit.Text = Properties.Resources.MenuTextExit;
+            menuItemExit.Click += new System.EventHandler(menuItemExit_Click);
+
+            notify1 = new NotifyIcon();
+            notify1.Icon = Properties.Resources.Icon1;
+            notify1.Text = Properties.Resources.Title;
+            notify1.Visible = true;
+            notify1.ContextMenuStrip = menu1;
+        }
+
+        ~Watcher()
+        {
+        }
+
+        private void watcher1_Created(object sender, FileSystemEventArgs e)
+        {
+            string tmpFileName = Path.GetFileNameWithoutExtension(e.FullPath);
+            string psFileName = Path.GetFileNameWithoutExtension(tmpFileName) + ".ps";
+            string dirName = Path.GetDirectoryName(e.FullPath);
+            try
+            {
+                File.Move(System.IO.Path.Combine(dirName, tmpFileName), System.IO.Path.Combine(dirName, psFileName));
+                File.Delete(e.FullPath);
+            }
+            catch (Exception ex)
+            {
+            }
+
+            string gsshell = null;
+            try
+            {
+                gsshell = (string)Microsoft.Win32.Registry.GetValue(Properties.Resources.RegKeyMariaPrintSystem, Properties.Resources.RegValuePsShell, "aa");
+            }
+            catch (Exception ex)
+            {
+                gsshell = null;
+            }
+            if (gsshell != null && gsshell != "")
+            {
+                gsshell = System.IO.Path.Combine(Application.StartupPath, gsshell);
+                if (System.IO.File.Exists(gsshell))
+                {
+                    try
+                    {
+                        System.Diagnostics.ProcessStartInfo psi = new System.Diagnostics.ProcessStartInfo();
+                        psi.FileName = gsshell;
+                        psi.Arguments = System.IO.Path.Combine(dirName, psFileName);
+                        System.Diagnostics.Process p = System.Diagnostics.Process.Start(psi);
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+            }
+        }
+
+        private void menuItemAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Properties.Resources.Title + "\r\n\r\n" + Properties.Resources.AutherCredit + "\r\n" + Properties.Resources.IconCredit);
+        }
+
+        private void menuItemExit_Click(object sender, EventArgs e)
+        {
+            notify1.Visible = false;
+            Application.Exit();
+        }
+    }
+
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Watcher watcher1 = new Watcher();
+
+            Application.Run();
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # MariaPrintSystem
+
+What is this?
+=============
+
+This is a print management system for Windows based on V3 printer model.
+
+Requirements and Limitations
+============================
+
+This program worked Windows with .NET 2.0 and later.
+
+Implemented
+-----------
+
+    Task tray icon/program for ps files watcher and the application kicker.
+
+Under the developing
+--------------------
+
+    Many
+
+License
+=======
+
+Copyright (c) 2018 @marbocub <marbocub@gmail.com>, All rights reserved.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License version 3 as published by the Free Software Foundation, see LISENCE.
+
+The program Icon
+================
+
+The Icon made by [Freepik][1] from [www.flaticon.com][2] is licensed by [CC 3.0 BY][3].
+[1]: http://www.freepik.com/
+[2]: https://www.flaticon.com/
+[3]: http://creativecommons.org/licenses/by/3.0/
