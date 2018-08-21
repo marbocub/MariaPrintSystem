be3d8a2296e591ee6233b21b82819f94f302ec7f
Mitsunori YOSHIDA <marbocub@gmail.com>
2018-08-22 07:35:21 +0900

    improve installer

--- MariaPrintConfigurator/MainForm.Designer.cs
+++ MariaPrintConfigurator/MainForm.Designer.cs
@@ -81,7 +81,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(170, 12);
             this.label1.TabIndex = 4;
-            this.label1.Text = "課金対象プリンタを選択してください";
+            this.label1.Text = "制御対象プリンタを選択してください";
             // 
             // checkedListBoxPrinters
             // 
